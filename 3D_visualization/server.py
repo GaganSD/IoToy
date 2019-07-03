a65052c1f75b1ae311a125a4bc690f7914705dbf
ayyGagan <gagandevagiri@gmail.com>
2019-07-03 16:40:19 +0100

    fixed bugs, added teddy, edited teddy

--- 3D_visualization/server.py
+++ 3D_visualization/server.py
@@ -5,25 +5,22 @@ import time
 
 from flask import *
 
-import board
-import busio
-import adafruit_bno055
+from Adafruit_BNO055 import BNO055
 
-i2c = busio.I2C(board.SCL, board.SDA)
-bno = adafruit_bno055.BNO055(i2c)
+bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 
-
-# How often to update the BNO sensor data (in hertz).
 BNO_UPDATE_FREQUENCY_HZ = 10
 CALIBRATION_FILE = 'calibration.json'
+BNO_AXIS_REMAP = { 'x': BNO055.AXIS_REMAP_X,
+                   'y': BNO055.AXIS_REMAP_Z,
+                   'z': BNO055.AXIS_REMAP_Y,
+                   'x_sign': BNO055.AXIS_REMAP_POSITIVE,
+                   'y_sign': BNO055.AXIS_REMAP_POSITIVE,
+                   'z_sign': BNO055.AXIS_REMAP_NEGATIVE }
 
 app = Flask(__name__)
 
-# Global state to keep track of the latest readings from the BNO055 sensor.
-# This will be accessed from multiple threads so care needs to be taken to
-# protect access with a lock (or else inconsistent/partial results might be read).
-# A condition object is used both as a lock for safe access across threads, and
-# to notify threads that the BNO state has changed.
+
 bno_data = {}
 bno_changed = threading.Condition()
 
@@ -38,13 +35,21 @@ def read_bno():
     it will never return!
     """
     while True:
+        # Grab new BNO sensor readings.
+        temp = bno.read_temp()
+        heading, roll, pitch = bno.read_euler()
+        x, y, z, w = bno.read_quaternion()
+        sys, gyro, accel, mag = bno.get_calibration_status()
+        status, self_test, error = bno.get_system_status(run_self_test=False)
+        if error != 0:
+            print 'Error! Value: {0}'.format(error)
         # Capture the lock on the bno_changed condition so the bno_data shared
         # state can be updated.
         with bno_changed:
-            bno_data['euler'] = bno.euler
-            bno_data['temp'] = bno.temperature
-            bno_data['quaternion'] = bno.quaternion
-            bno_data['calibration'] = bno.calibration_status
+            bno_data['euler'] = (heading, roll, pitch)
+            bno_data['temp'] = temp
+            bno_data['quaternion'] = (x, y, z, w)
+            bno_data['calibration'] = (sys, gyro, accel, mag)
             # Notify any waiting threads that the BNO state has been updated.
             bno_changed.notifyAll()
         # Sleep until the next reading.
@@ -85,6 +90,10 @@ def start_bno_thread():
     # See this SO question for more context:
     #   http://stackoverflow.com/questions/24617795/starting-thread-while-running-flask-with-debug
     global bno_thread
+    # Initialize BNO055 sensor.
+    if not bno.begin():
+        raise RuntimeError('Failed to initialize BNO055!')
+    bno.set_axis_remap(**BNO_AXIS_REMAP)
     # Kick off BNO055 reading thread.
     bno_thread = threading.Thread(target=read_bno)
     bno_thread.daemon = True  # Don't let the BNO reading thread block exiting.
@@ -99,17 +108,23 @@ def bno_path():
 @app.route('/save_calibration', methods=['POST'])
 def save_calibration():
     # Save calibration data to disk.
-    #
-    # TODO: implement this
-    #
+    # First grab the lock on BNO sensor access to make sure nothing else is
+    # writing to the sensor right now.
+    with bno_changed:
+        data = bno.get_calibration()
+    # Write the calibration to disk.
+    with open(CALIBRATION_FILE, 'w') as cal_file:
+        json.dump(data, cal_file)
     return 'OK'
 
 @app.route('/load_calibration', methods=['POST'])
 def load_calibration():
     # Load calibration from disk.
-    #
-    # TODO: implement this
-    #
+    with open(CALIBRATION_FILE, 'r') as cal_file:
+        data = json.load(cal_file)
+    # Grab the lock on BNO sensor access to serial access to the sensor.
+    with bno_changed:
+        bno.set_calibration(data)
     return 'OK'
 
 @app.route('/')
