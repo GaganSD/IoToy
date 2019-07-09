8d873dc5d61a15bd588e92a3319ce77ca1a63255
ayyGagan <gagandevagiri@gmail.com>
2019-07-09 14:35:46 +0100

    Added working files from develop, made recievePi4.py

---
+++ 3D_visualization/recievePi4.py
@@ -0,0 +1,119 @@
+import json
+import logging
+import threading
+import time
+
+from flask import *
+
+from Adafruit_BNO055 import BNO055
+
+bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+
+BNO_UPDATE_FREQUENCY_HZ = 10
+CALIBRATION_FILE = 'calibration.json'
+BNO_AXIS_REMAP = { 'x': BNO055.AXIS_REMAP_X,
+                   'y': BNO055.AXIS_REMAP_Z,
+                   'z': BNO055.AXIS_REMAP_Y,
+                   'x_sign': BNO055.AXIS_REMAP_POSITIVE,
+                   'y_sign': BNO055.AXIS_REMAP_POSITIVE,
+                   'z_sign': BNO055.AXIS_REMAP_NEGATIVE }
+
+app = Flask(__name__)
+
+
+bno_data = {}
+bno_changed = threading.Condition()
+
+# Background thread to read BNO sensor data.  Will be created right before
+# the first request is served (see start_bno_thread below).
+bno_thread = None
+
+
+def read_bno():
+    """Function to read the BNO sensor and update the bno_data object with the
+    latest BNO orientation, etc. state.  Must be run in its own thread because
+    it will never return!
+    """
+    while True:
+        # Grab new BNO sensor readings.
+        temp = bno.read_temp()
+        heading, roll, pitch = bno.read_euler()
+        x, y, z, w = bno.read_quaternion()
+        sys, gyro, accel, mag = bno.get_calibration_status()
+        status, self_test, error = bno.get_system_status(run_self_test=False)
+        if error != 0:
+            print 'Error! Value: {0}'.format(error)
+        # Capture the lock on the bno_changed condition so the bno_data shared
+        # state can be updated.
+        with bno_changed:
+            bno_data['euler'] = (heading, roll, pitch)
+            bno_data['temp'] = temp
+            bno_data['quaternion'] = (x, y, z, w)
+            bno_data['calibration'] = (sys, gyro, accel, mag)
+            # Notify any waiting threads that the BNO state has been updated.
+            bno_changed.notifyAll()
+        # Sleep until the next reading.
+        time.sleep(1.0/BNO_UPDATE_FREQUENCY_HZ)
+
+def bno_sse():
+    """Function to handle sending BNO055 sensor data to the client web browser
+    using HTML5 server sent events (aka server push).  This is a generator function
+    that flask will run in a thread and call to get new data that is pushed to
+    the client web page.
+    """
+    # Loop forever waiting for a new BNO055 sensor reading and sending it to
+    # the client.  Since this is a generator function the yield statement is
+    # used to return a new result.
+    while True:
+        # Capture the bno_changed condition lock and then wait for it to notify
+        # a new reading is available.
+        with bno_changed:
+            bno_changed.wait()
+            # A new reading is available!  Grab the reading value and then give
+            # up the lock.
+            heading, roll, pitch = bno_data['euler']
+            temp = bno_data['temp']
+            x, y, z, w = bno_data['quaternion']
+            sys, gyro, accel, mag = bno_data['calibration']
+        # Send the data to the connected client in HTML5 server sent event format.
+        data = {'heading': heading, 'roll': roll, 'pitch': pitch, 'temp': temp,
+                'quatX': x, 'quatY': y, 'quatZ': z, 'quatW': w,
+                'calSys': sys, 'calGyro': gyro, 'calAccel': accel, 'calMag': mag }
+        yield 'data: {0}\n\n'.format(json.dumps(data))
+
+
+@app.before_first_request
+def start_bno_thread():
+    # Start the BNO thread right before the first request is served.  This is
+    # necessary because in debug mode flask will start multiple main threads so
+    # this is the only spot to put code that can only run once after starting.
+    # See this SO question for more context:
+    #   http://stackoverflow.com/questions/24617795/starting-thread-while-running-flask-with-debug
+    global bno_thread
+    # Initialize BNO055 sensor.
+    if not bno.begin():
+        raise RuntimeError('Failed to initialize BNO055!')
+    bno.set_axis_remap(**BNO_AXIS_REMAP)
+    # Kick off BNO055 reading thread.
+    bno_thread = threading.Thread(target=read_bno)
+    bno_thread.daemon = True  # Don't let the BNO reading thread block exiting.
+    bno_thread.start()
+
+@app.route('/bno')
+def bno_path():
+    # Return SSE response and call bno_sse function to stream sensor data to
+    # the webpage.
+    return Response(bno_sse(), mimetype='text/event-stream')
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Create a server listening for external connections on the default
+    # port 5000.  Enable debug mode for better error messages and live
+    # reloading of the server on changes.  Also make the server threaded
+    # so multiple connections can be processed at once (very important
+    # for using server sent events).
+    app.run(host='0.0.0.0', debug=True, threaded=True)
---
+++ 3D_visualization/sendFromPi3.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+import time
+import paho.mqtt.publish as publish
+from Adafruit_BNO055 import BNO055
+
+bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+
+if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+    logging.basicConfig(level=logging.DEBUG)
+
+# Initialize the BNO055 and stop if something went wrong.
+if not bno.begin():
+    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
+# Print system status and self test result.
+status, self_test, error = bno.get_system_status()
+print('System status: {0}'.format(status))
+print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+# Print out an error if system status is in error mode.
+if status == 0x01:
+    print('System error: {0}'.format(error))
+    print('See datasheet section 4.3.59 for the meaning.')
+
+# Print BNO055 software revision and other diagnostic data.
+sw, bl, accel, mag, gyro = bno.get_revision()
+
+print('Reading BNO055 data, press Ctrl-C to quit...')
+
+while True:
+    heading, roll, pitch = bno.read_euler()
+
+    # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+    sys, gyro, accel, mag = bno.get_calibration_status()
+    x, y, z, w = bno.read_quaternion()
+    # Heading, Roll, Pitch, sys_cal, gyro_cal, accel_cal, mag_cal.
+    accel = bno.read_linear_acceleration()
+    magnitude = (accel[0]**2 +  accel[1]**2 + accel[2]**2)**(1/2)
+
+    publish.single("test/accdata",'{0} {1} {2} {3:.2f} {4:.2f} {5} {6} {7} {8} {9} {10}'.format(heading, roll, pitch, sys, gyro, magnitude, mag, x, y, z, w),hostname="192.168.5.1" )
+    time.sleep(1)
