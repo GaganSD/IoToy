e24525cb3685e95cfa01c33ec1541e059e847409
ayyGagan <gagandevagiri@gmail.com>
2019-07-10 14:30:15 +0100

    Made code readable, removed print functions, documented code

--- 3D_visualization/recievePi4.py
+++ 3D_visualization/recievePi4.py
@@ -1,3 +1,10 @@
+######
+## IoToy made by Gagan Devagiri for University of Edinburgh's
+## IoT Research and Innovation Service
+## v1.0 - 10/07/19
+######
+
+
 import json
 import logging
 import threading
@@ -6,33 +13,20 @@ from flask import *
 import paho.mqtt.client as mqtt
 from Adafruit_BNO055 import BNO055
 
-# bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
-
-BNO_UPDATE_FREQUENCY_HZ = 10
-CALIBRATION_FILE = 'calibration.json'
-BNO_AXIS_REMAP = { 'x': BNO055.AXIS_REMAP_X,
-                   'y': BNO055.AXIS_REMAP_Z,
-                   'z': BNO055.AXIS_REMAP_Y,
-                   'x_sign': BNO055.AXIS_REMAP_POSITIVE,
-                   'y_sign': BNO055.AXIS_REMAP_POSITIVE,
-                   'z_sign': BNO055.AXIS_REMAP_NEGATIVE }
-
-app = Flask(__name__)
-
-
-bno_data = {}
-bno_changed = threading.Condition()
-
-# Background thread to read BNO sensor data.  Will be created right before
-# the first request is served (see start_bno_thread below).
-bno_thread = None
 
 global datalst, recievedCheck
 
+BNO_UPDATE_FREQUENCY_HZ = 10
+app = Flask(__name__)
+
+bno_data = {}
+bno_changed = threading.Condition()
+bno_thread = None
 recieved_check = False
 
+
 def send_bno():
-    time.sleep(2)
+    time.sleep(3)
     global datalst
     while True:
         heading = float(datalst[0])
@@ -56,9 +50,9 @@ def send_bno():
     
 
 def read_bno():
-    """Function to read the BNO sensor and update the bno_data object with the
-    latest BNO orientation, etc. state.  Must be run in its own thread because
-    it will never return!
+    """
+    Connects to the mqtt client & calls on_message
+    updates dataSTR variable
     """
     def on_message(client, user_data, message):
         dataSTR = str(message.payload.decode())
@@ -73,31 +67,18 @@ def read_bno():
 
 
 def bno_sse():
-    """Function to handle sending BNO055 sensor data to the client web browser
-    using HTML5 server sent events (aka server push).  This is a generator function
-    that flask will run in a thread and call to get new data that is pushed to
-    the client web page.
     """
-    # Loop forever waiting for a new BNO055 sensor reading and sending it to
-    # the client.  Since this is a generator function the yield statement is
-    # used to return a new result.
-    print("inside bno_sse")
+    Generator Function. Sends sensor data to client web browser using
+    server push.
+    """
     while True:
-        # Capture the bno_changed condition lock and then wait for it to notify
-        # a new reading is available.
         with bno_changed:
-            print("inside while loop")
             bno_changed.wait()
-            print("inside bno_changed")
-            # A new reading is available!  Grab the reading value and then give
-            # up the lock.
             bno_data['euler']
             heading, roll, pitch = bno_data['euler']
             temp = bno_data['temp']
             x, y, z, w = bno_data['quaternion']
             sys, gyro, accel, mag = bno_data['calibration']
-            print(bno_data)
-            print("sending bno_data")
         # Send the data to the connected client in HTML5 server sent event format.
         data = {'heading': heading, 'roll': roll, 'pitch': pitch, 'temp': temp,
                 'quatX': x, 'quatY': y, 'quatZ': z, 'quatW': w,
@@ -107,21 +88,20 @@ def bno_sse():
 
 @app.before_first_request
 def start_bno_thread():
+
     # Start the BNO thread right before the first request is served.  This is
     # necessary because in debug mode flask will start multiple main threads so
     # this is the only spot to put code that can only run once after starting.
     # See this SO question for more context:
     #   http://stackoverflow.com/questions/24617795/starting-thread-while-running-flask-with-debug
+
     global bno_thread
-    # Initialize BNO055 sensor.
-#    if not bno.begin():
-#        raise RuntimeError('Failed to initialize BNO055!')
-#    bno.set_axis_remap(**BNO_AXIS_REMAP)
-    # Kick off BNO055 reading thread.
     bno_thread = threading.Thread(target=read_bno)
     sending_thread = threading.Thread(target=send_bno)
+
     sending_thread.daemon = True
-    bno_thread.daemon = True  # Don't let the BNO reading thread block exiting.
+    bno_thread.daemon = True
+
     sending_thread.start()
     bno_thread.start()
 
@@ -136,10 +116,5 @@ def root():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    # Create a server listening for external connections on the default
-    # port 5000.  Enable debug mode for better error messages and live
-    # reloading of the server on changes.  Also make the server threaded
-    # so multiple connections can be processed at once (very important
-    # for using server sent events).
+if __name__ == '__main__':    
     app.run(host='0.0.0.0', debug=True, threaded=True)
--- 3D_visualization/sendPi0.py
+++ 3D_visualization/sendPi0.py
@@ -1,15 +1,22 @@
+#####
+## IoToy made by Gagan Devagiri for University
+## of Edinburgh's IoT Research and Innovation service
+## v1.0 - 10/07/19
+#####
+
 import logging
 import sys
 import time
 import paho.mqtt.publish as publish
 from Adafruit_BNO055 import BNO055
+import numpy as np
+
 
 bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 
 if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
     logging.basicConfig(level=logging.DEBUG)
 
-# Initialize the BNO055 and stop if something went wrong.
 if not bno.begin():
     raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
@@ -17,25 +24,25 @@ if not bno.begin():
 status, self_test, error = bno.get_system_status()
 print('System status: {0}'.format(status))
 print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
-# Print out an error if system status is in error mode.
+
 if status == 0x01:
     print('System error: {0}'.format(error))
     print('See datasheet section 4.3.59 for the meaning.')
 
-# Print BNO055 software revision and other diagnostic data.
 sw, bl, accel, mag, gyro = bno.get_revision()
 
 print('Reading BNO055 data, press Ctrl-C to quit...')
 
 while True:
-    heading, roll, pitch = bno.read_euler()
 
-    # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+    heading, roll, pitch = bno.read_euler()
     sys, gyro, accel, mag = bno.get_calibration_status()
     x, y, z, w = bno.read_quaternion()
-    # Heading, Roll, Pitch, sys_cal, gyro_cal, accel_cal, mag_cal.
-    #accel = bno.read_linear_acceleration()
-    #magnitude = (accel[0]**2 +  accel[1]**2 + accel[2]**2)**(1/2)
 
-    publish.single("test/accdata",'{0} {1} {2} {3:.2f} {4:.2f} {5} {6} {7} {8} {9} {10}'.format(heading, roll, pitch, sys, gyro, accel, mag, x, y, z, w),hostname="192.168.5.1" )
+    xuData  = bno.read_linear_acceleration()
+    magnitude = np.sqrt((np.square(xuData[0]) + np.square(xuData[1]) + np.square(xuData[2])))
+
+    publish.single("test/accdata",'{0} {1} {2} {3:.2f} {4:.2f} {5} {6} {7} {8} {9} {10} {11}'.format(heading, roll, pitch, sys, gyro, accel, mag, x, y, z, w, magnitude),hostname="192.168.5.1" )
+    publish.single("test/accdata1", magnitude, hostname="192.168.5.1")
+
     time.sleep(1)
